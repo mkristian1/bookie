cb445e61c7b1fbe6673639589cdc693da7f0bee3
desu_kris <mkrtchyan_k@garpix.com>
2022-05-02 11:21:39 +0400

    Added count down

--- src/components/timer/index.js
+++ src/components/timer/index.js
@@ -1,26 +1,34 @@
+import Countdown from 'react-countdown';
 import styles from './scss/index.module.scss'
 
 const Timer = () => {
+    const renderer = ({ formatted }) => {
+        return (
+            <>
+                <div className={styles['timer__item']}>
+                    <time className={styles['timer__value']}>{formatted.days}</time>
+                    <p className={styles['timer__value_type']}>days</p>
+                </div>
+                <div className={styles['timer__item']}>
+                    <time className={styles['timer__value']}>{formatted.hours}</time>
+                    <p className={styles['timer__value_type']}>hrs</p>
+                </div>
+                <div className={styles['timer__item']}>
+                    <time className={styles['timer__value']}>{formatted.minutes}</time>
+                    <p className={styles['timer__value_type']}>min</p>
+                </div>
+                <div className={styles['timer__item']}>
+                    <time className={styles['timer__value']}>{formatted.seconds}</time>
+                    <p className={styles['timer__value_type']}>sec</p>
+                </div>
+            </>
+        )
+    }
     return (
         <div className={styles['timer']}>
             <h2 className={styles['timer__title']}>Time before launch</h2>
             <div className={styles['time-wrap']}>
-                <div className={styles['timer__item']}>
-                    <time className={styles['timer__value']}>00</time>
-                    <p className={styles['timer__value_type']}>days</p>
-                </div>
-                <div className={styles['timer__item']}>
-                    <time className={styles['timer__value']}>00</time>
-                    <p className={styles['timer__value_type']}>hrs</p>
-                </div>
-                <div className={styles['timer__item']}>
-                    <time className={styles['timer__value']}>00</time>
-                    <p className={styles['timer__value_type']}>min</p>
-                </div>
-                <div className={styles['timer__item']}>
-                    <time className={styles['timer__value']}>00</time>
-                    <p className={styles['timer__value_type']}>sec</p>
-                </div>
+                <Countdown renderer={renderer} date={Date.now() + 800000000} />
             </div>
         </div>
     )
